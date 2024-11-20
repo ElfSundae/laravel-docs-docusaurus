a9af38431bafa1726f78962df9c606075b7ed6bb
Elf Sundae <elf.sundae@gmail.com>
2024-11-21 07:06:54 +0800

    Change config object  to createConfigAsync function

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,194 +4,194 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'Laravel - The PHP Framework For Web Artisans',
-  tagline: 'Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.',
-  favicon: 'img/favicon.ico',
+export default async function createConfigAsync() {
+  /** @type {import('@docusaurus/types').Config} */
+  return {
+    title: 'Laravel - The PHP Framework For Web Artisans',
+    tagline: 'Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://laravel.0x123.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://laravel.0x123.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'facebook', // Usually your GitHub org/user name.
+    projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  trailingSlash: false,
+    trailingSlash: false,
 
-  markdown: {
-    format: 'detect',
-  },
-
-  titleDelimiter: '-',
-
-  headTags: [
-    {
-      tagName: 'link',
-      attributes: {
-        rel: 'apple-touch-icon',
-        sizes: '150x150',
-        href: 'img/apple-touch-icon.png',
-      },
+    markdown: {
+      format: 'detect',
     },
-  ],
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh'],
-    localeConfigs: {
-      zh: {
-        label: '简体中文',
-        htmlLang: 'zh-CN',
-      },
-    },
-  },
+    titleDelimiter: '-',
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: ({ version, docPath }) => {
-            return `https://github.com/laravel/docs/edit/${version}/${docPath}`;
-          },
-          versions: {
-            current: {
-              label: 'Master',
-              path: 'master',
-            },
-            // '11.x': {
-            //   banner: 'none',
-            //   path: '11.x',
-            // },
-          },
+    headTags: [
+      {
+        tagName: 'link',
+        attributes: {
+          rel: 'apple-touch-icon',
+          sizes: '150x150',
+          href: 'img/apple-touch-icon.png',
         },
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
+      },
+    ],
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+      defaultLocale: 'en',
+      locales: ['en', 'zh'],
+      localeConfigs: {
+        zh: {
+          label: '简体中文',
+          htmlLang: 'zh-CN',
+        },
+      },
+    },
+
+    presets: [
+      [
+        'classic',
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: './sidebars.js',
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl: ({ version, docPath }) => {
+              return `https://github.com/laravel/docs/edit/${version}/${docPath}`;
+            },
+            versions: {
+              current: {
+                label: 'Master',
+                path: 'master',
+              },
+              // '11.x': {
+              //   banner: 'none',
+              //   path: '11.x',
+              // },
+            },
+          },
+          blog: false,
+          theme: {
+            customCss: './src/css/custom.css',
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        // Replace with your project's social card
+        image: 'img/social-card.jpg',
+        navbar: {
+          title: 'Laravel',
+          logo: {
+            alt: 'Laravel Logo',
+            src: 'img/logo.svg',
+          },
+          items: [
+            {
+              type: 'docSidebar',
+              sidebarId: 'tutorialSidebar',
+              position: 'left',
+              label: 'Tutorial',
+            },
+            {
+              label: 'Documentation',
+              to: '/docs/installation',
+              position: 'left',
+            },
+            {
+              label: 'API',
+              to: 'pathname:///api/',
+              position: 'left',
+            },
+            {
+              type: 'docsVersionDropdown',
+              position: 'right',
+            },
+            {
+              type: 'localeDropdown',
+              position: 'right',
+            },
+            {
+              href: 'https://github.com/laravel',
+              className: 'header-github-link',
+              position: 'right',
+            },
+            {
+              type: 'search',
+              position: 'right',
+            },
+          ],
+        },
+        footer: {
+          style: 'dark',
+          links: [
+            {
+              title: 'Docs',
+              items: [
+                {
+                  label: 'Tutorial',
+                  to: '/docs/intro',
+                },
+              ],
+            },
+            {
+              title: 'Community',
+              items: [
+                {
+                  label: 'Stack Overflow',
+                  href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                },
+                {
+                  label: 'Discord',
+                  href: 'https://discordapp.com/invite/docusaurus',
+                },
+                {
+                  label: 'X',
+                  href: 'https://x.com/docusaurus',
+                },
+              ],
+            },
+            {
+              title: 'More',
+              items: [
+                {
+                  label: 'Blog',
+                  to: '/blog',
+                },
+                {
+                  label: 'GitHub',
+                  href: 'https://github.com/facebook/docusaurus',
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        },
+        prism: {
+          theme: prismThemes.github,
+          darkTheme: prismThemes.dracula,
+        },
+        colorMode: {
+          respectPrefersColorScheme: true,
         },
       }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/social-card.jpg',
-      navbar: {
-        title: 'Laravel',
-        logo: {
-          alt: 'Laravel Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {
-            label: 'Documentation',
-            to: '/docs/installation',
-            position: 'left',
-          },
-          {
-            label: 'API',
-            to: 'pathname:///api/',
-            position: 'left',
-          },
-          {
-            type: 'docsVersionDropdown',
-            position: 'right',
-          },
-          {
-            type: 'localeDropdown',
-            position: 'right',
-          },
-          {
-            href: 'https://github.com/laravel',
-            className: 'header-github-link',
-            position: 'right',
-          },
-          {
-            type: 'search',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-      colorMode: {
-        respectPrefersColorScheme: true,
-      },
-    }),
-};
-
-export default config;
+  };
+}
